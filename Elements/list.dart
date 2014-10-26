7fc2dbac8d3a2eafd821ba89855d914d641cb242
syuilo <syuilotan@yahoo.co.jp>
2014-10-26 19:38:38 +0900

    リストの入れ子に対応
    -Item
    -Item
    --Item
    --Item
    --Item
    
    -Item
    -1.Item
    -1.Item
    -1.Item
    -1.-Item
    -1.-Item
    -1.-Item

--- Elements/list.dart
+++ Elements/list.dart
@@ -39,8 +39,28 @@ class EList extends Element {
 	       Token startToken = parser.scanner.read();
 		EList list = new EList();
 		list.parent = parent;
-		list.type = parser.scanner.read(1).token == 'number' ? 'ordered' : 'unordered';
 		parser.scanner.next();
+		int hierarchy = 0;
+		parser.scanner.scan((Token token) {
+			if (token.token == 'hyphen') {
+				hierarchy++;
+				list.type = 'unordered';
+			} else if (token.token == 'number' && parser.scanner.pick(token.id + 1).token == 'period') {
+				hierarchy++;
+                               	list.type = 'ordered';
+                               	parser.scanner.next();
+			} else if (token.token == 'space') {
+				
+			} else {
+				return true;
+			}
+			parser.scanner.next();
+		});
+		if (list.type == 'ordered') {
+			parser.scanner.back(2);
+		} else {
+			parser.scanner.back();
+		}
 		parser.scanner.scan((Token token) {
 			if (list.type == 'ordered') {
 				parser.scanner.next(2);
@@ -48,9 +68,49 @@ class EList extends Element {
 				parser.scanner.next();
 			}
 			EListItem item = new EListItem();
+			item.parent = list;
+			EList childList;
+			//var beforeToken = parser.scanner.read();
+			bool exit = false;
+			//var afterToken = parser.scanner.read();
 			item.children = parser.analyze(list, (token) {
+				if (
+        				parser.scanner.pick(token.id).token == 'newline' && (
+        					parser.scanner.pick(token.id + 1).token == 'hyphen' || (
+        						parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period'
+        					)
+        				)
+        			) {
+                			parser.scanner.next();
+                			var beforeToken = parser.scanner.read();
+        				int nextHierarchy = getNextHierarchy(parser);
+        				int nextStep = getNextStep(parser);
+        				var afterToken = parser.scanner.read();
+        		       		if (hierarchy > nextHierarchy) {
+        		       			exit = true;
+                               			return true;
+                               		} else if (hierarchy == nextHierarchy) {
+                        			if (list.type == 'ordered') {
+                        				parser.scanner.next(nextStep - 3);
+                        			} else {
+                        				parser.scanner.next(nextStep - 2);
+                        			}
+                               		} else {
+                               			parser.scanner.back();
+                               			childList = new EList();
+                               			childList.parent = item;
+                        			childList = EList.generate(parser, childList);
+                               		}
+        			}
 				return token.token == 'newline';
 			}, ['paragraph']);
+			if (exit) {
+				list.children.add(item);
+				return true;
+			}
+			if (childList != null) {
+				item.children.add(childList);
+			}
 			list.children.add(item);
 			if (parser.scanner.read(1).token == 'newline') {
 				parser.scanner.next();
@@ -66,5 +126,52 @@ class EList extends Element {
 		});
 		parser.scanner.back();
 		return list;
+	}
+       
+	static int getNextHierarchy(Parser parser) {
+		int step = 0;
+		int nextHierarchy = 0;
+		parser.scanner.scan((Token token) {
+			if (token.token == 'hyphen') {
+				nextHierarchy++;
+			} else if (token.token == 'number' && parser.scanner.pick(token.id + 1).token == 'period') {
+				nextHierarchy++;
+				step++;
+				parser.scanner.next();
+			} else if (token.token == 'space') {
+				
+			} else {
+				return true;
+			}
+			step++;
+			parser.scanner.next();
+		});
+		// 階層を調べるために進めたトークンリーダを元の位置まで巻き戻す
+       		parser.scanner.back(step);
+       		return nextHierarchy;
+	}
+	
+	static int getNextStep(Parser parser) {
+		int step = 0;
+		int nextStep = 0;
+		parser.scanner.scan((Token token) {
+			if (token.token == 'hyphen') {
+				nextStep++;
+			} else if (token.token == 'number' && parser.scanner.pick(token.id + 1).token == 'period') {
+				nextStep++;
+				nextStep++;
+				step++;
+				parser.scanner.next();
+			} else if (token.token == 'space') {
+				
+			} else {
+				return true;
+			}
+			step++;
+			parser.scanner.next();
+		});
+		// 階層を調べるために進めたトークンリーダを元の位置まで巻き戻す
+       		parser.scanner.back(step);
+       		return nextStep;
 	}
 }

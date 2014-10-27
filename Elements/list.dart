67db61a11ce32f4764b957bb18f02149bde111d1
syuilo <syuilotan@yahoo.co.jp>
2014-10-27 12:53:31 +0900

    リストの入れ子問題を修正
    (ただし自分でも何故動くのか完全には理解できていない)

--- Elements/list.dart
+++ Elements/list.dart
@@ -42,6 +42,8 @@ class EList extends Element {
 		list.parent = parent;
 		parser.scanner.next();
 		int hierarchy = 0;
+		
+		// リストタイプと階層取得
 		parser.scanner.scan((Token token) {
 			if (token.token == 'hyphen') {
 				hierarchy++;
@@ -57,88 +59,89 @@ class EList extends Element {
 			}
 			parser.scanner.next();
 		});
-		if (list.type == 'ordered') {
-			parser.scanner.back(2);
-		} else {
-			parser.scanner.back();
-		}
 		
-		parser.scanner.scan((Token token) {
-			if (list.type == 'ordered') {
-				parser.scanner.next(2);
-			} else {
-				parser.scanner.next();
-			}
-			EListItem item = new EListItem();
-			item.parent = list;
-			EList childList;
-			String childListType = '';
-			bool exit = false;
-			int nextHierarchy = 0;
-			item.children = parser.analyze(item, (token) {
-				if (
-					parser.scanner.pick(token.id).token == 'newline' && (
-						parser.scanner.pick(token.id + 1).token == 'hyphen' || (
-        						parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period'
-        					)
-        				)
-        			) {
-					if (parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period') {
-						childListType = 'ordered';
-						parser.scanner.next(2);
-					} else {
-						childListType = 'unordered';
-						parser.scanner.next();
-					}
-        				nextHierarchy = getNextHierarchy(parser);
-        				int nextStep = getNextStep(parser);
-        		       		if (hierarchy > nextHierarchy) {
-        		       			// 昇る時
-        		       			parser.scanner.back(2);
-        		       			exit = true;
-                               			return true;
-                               		} else if (hierarchy == nextHierarchy) {
-                               			// 兄弟
-                        			if (list.type == 'ordered') {
-                        				parser.scanner.next(nextStep - 3);
-                        			} else {
-                        				parser.scanner.next(nextStep - 2);
-                        			}
-                               		} else { // hierarchy < nextHierarchy
-                               			// 潜る時
-                               			parser.scanner.back();
-                               			childList = new EList();
-                               			childList.parent = item;
-                        			childList = EList.generate(parser, childList);
-                               		}
-        			}
-				return token.token == 'newline';
-			}, ['paragraph']);
-			if (exit) {
-				if (item.children.length != 0) {
-					list.children.add(item);
-				}
-				return true;
-			}
-			if (childList != null) {
-				childList.parent = item;
-				item.children.add(childList);
-			}
-			if (item.children.length != 0) {
-				list.children.add(item);
-			}
-			if (parser.scanner.read(1).token == 'newline') {
-				parser.scanner.next();
-				return true;
-			}
-			parser.scanner.next();
-		}, () {
-			parser.addError(new Error(
-				'${startToken.row}行目の${startToken.col}列目あたりから始まった List(- または 数字.) が閉じていません。',
-				startToken.row,
-				startToken.col
-				));
-		});
+		// アイテム走査
+   		parser.scanner.scan((Token token) {
+      			EListItem item = new EListItem();
+      			item.parent = list;
+      			EList childList;
+      			String childListType = '';
+      			bool exit = false;
+      			int nextHierarchy = 0;
+      			
+      			bool isDummy = false;
+			// アイテムの子供走査
+   			item.children = parser.analyze(item, (token) {
+   				if (parser.scanner.pick(token.id).token == 'dummy') {
+   					isDummy = true;
+   				}
+   				// 次のアイテム
+   				if (
+   					parser.scanner.pick(token.id).token == 'newline' && (
+   						parser.scanner.pick(token.id + 1).token == 'hyphen' || (
+           						parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period'
+           					)
+           				)
+           			) {
+   					if (parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period') {
+   						childListType = 'ordered';
+   					} else {
+   						childListType = 'unordered';
+   					}
+   					parser.scanner.next();
+           				nextHierarchy = getNextHierarchy(parser);
+           				int nextStep = getNextStep(parser);
+           				parser.scanner.back();
+           				
+					// この時点でトークンリーダはnewlineの位置
+           		       		if (hierarchy > nextHierarchy) {
+           		       			// 昇る時
+           		       			parser.scanner.back();
+           		       			parser.scanner.tokens[parser.scanner.read().id].token = 'dummy';
+   						// アイテムの子供走査終了 (@owariへ)
+           		       			exit = true;
+                                  		return true;
+                                  	} else if (hierarchy == nextHierarchy) {
+                          			// 兄弟アイテム
+                           			parser.scanner.next(nextStep);
+                          		} else { // hierarchy < nextHierarchy
+                          			// 潜る時 (子リスト)
+                          			childList = new EList();
+                          			childList.parent = item;
+                           			childList = EList.generate(parser, childList);
+                                  	}
+           			}
+   				if (token.token == 'newline') {
+           				return true;
+           			}
+   				return false;
+   			}, ['paragraph']);
+   			
+   			// @owari
+   			if (exit) {
+   				if (!isDummy)
+   				list.children.add(item);
+   				return true;
+   			}
+   			if (childList != null) {
+   				childList.parent = item;
+   				item.children.add(childList);
+   			}
+   			if (!isDummy)
+   			list.children.add(item);
+   			if (parser.scanner.read(1).token == 'newline') {
+   				parser.scanner.next();
+   				return true;
+   			}
+   			parser.scanner.next();
+   		}, () {
+   			parser.addError(new Error(
+   				'${startToken.row}行目の${startToken.col}列目あたりから始まった List(- または 数字.) が閉じていません。',
+   				startToken.row,
+   				startToken.col
+   				));
+   		});
+		
 		parser.scanner.back();
 		return list;
 	}
@@ -189,4 +192,126 @@ class EList extends Element {
        		parser.scanner.back(step);
        		return nextStep;
 	}
+	/*
+	  static EList generate(Parser parser, Element parent) {
+      	       Token startToken = parser.scanner.read();
+      		EList list = new EList();
+      		list.parent = parent;
+      		parser.scanner.next();
+      		int hierarchy = 0;
+      		
+      		// リストタイプ取得
+      		parser.scanner.scan((Token token) {
+      			if (token.token == 'hyphen') {
+      				hierarchy++;
+      				list.type = 'unordered';
+      			} else if (token.token == 'number' && parser.scanner.pick(token.id + 1).token == 'period') {
+      				hierarchy++;
+                                     	list.type = 'ordered';
+                                     	parser.scanner.next();
+      			} else if (token.token == 'space') {
+      				
+      			} else {
+      				return true;
+      			}
+      			parser.scanner.next();
+      		});
+      		if (list.type == 'ordered') {
+      			parser.scanner.back(2);
+      		} else {
+      			parser.scanner.back();
+      		}
+      		
+      		// アイテム走査
+      		parser.scanner.scan((Token token) {
+      			if (list.type == 'ordered') {
+      				parser.scanner.next(2);
+      			} else {
+      				parser.scanner.next();
+      			}
+      			EListItem item = new EListItem();
+      			item.parent = list;
+      			EList childList;
+      			String childListType = '';
+      			bool exit = false;
+      			int nextHierarchy = 0;
+      			
+      			// アイテムの子供走査
+      			item.children = parser.analyze(item, (token) {
+      				// 次のアイテム
+      				if (
+      					parser.scanner.pick(token.id).token == 'newline' && (
+      						parser.scanner.pick(token.id + 1).token == 'hyphen' || (
+              						parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period'
+              					)
+              				)
+              			) {
+      					if (parser.scanner.pick(token.id + 1).token == 'number' && parser.scanner.pick(token.id + 2).token == 'period') {
+      						childListType = 'ordered';
+      					} else {
+      						childListType = 'unordered';
+      					}
+      					parser.scanner.next();
+              				nextHierarchy = getNextHierarchy(parser);
+              				int nextStep = getNextStep(parser);
+              		       		if (hierarchy > nextHierarchy) {
+              		       			// 昇る時
+              		       			// この時点でトークンリーダはnewlineの次の位置
+              		       			if (list.type == 'ordered') {
+              		       				parser.scanner.back();
+      	                			} else {
+      	                				
+      	                			}
+              		       			parser.scanner.back(2);
+      						// アイテムの子供走査終了 (@owariへ)
+              		       			exit = true;
+                                     			return true;
+                                     		} else if (hierarchy == nextHierarchy) {
+                                     			// 兄弟
+                              			if (list.type == 'ordered') {
+                              				parser.scanner.next(nextStep - 3);
+                              			} else {
+                              				parser.scanner.next(nextStep - 2);
+                              			}
+                                     		} else { // hierarchy < nextHierarchy
+                                     			// 潜る時
+                                     			parser.scanner.back();
+                                     			childList = new EList();
+                                     			childList.parent = item;
+                              			childList = EList.generate(parser, childList);
+                                     		}
+              			}
+      				return token.token == 'newline';
+      			}, ['paragraph']);
+      			
+      			// @owari
+      			if (exit) {
+      				if (item.children.length != 0) {
+      					list.children.add(item);
+      				}
+      				return true;
+      			}
+      			if (childList != null) {
+      				childList.parent = item;
+      				item.children.add(childList);
+      			}
+      			if (item.children.length != 0) {
+      				list.children.add(item);
+      			}
+      			if (parser.scanner.read(1).token == 'newline') {
+      				parser.scanner.next();
+      				return true;
+      			}
+      			parser.scanner.next();
+      		}, () {
+      			parser.addError(new Error(
+      				'${startToken.row}行目の${startToken.col}列目あたりから始まった List(- または 数字.) が閉じていません。',
+      				startToken.row,
+      				startToken.col
+      				));
+      		});
+      		parser.scanner.back();
+      		return list;
+      	}
+	  */
 }
